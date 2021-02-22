9b545bdc244a66cf20ff3c181fd8db22b3390972
Abdullahi Odesanmi <56407386+realabdullah@users.noreply.github.com>
2021-02-22 01:23:25 +0100

    javascript for the modals/pop-ups by @realabdullah

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,3 +14,35 @@ function backProject() {
         openClose.style.display = "block"
     }
 }
+function chosePledge() {
+    const pledgeit = document.getElementById("selectPledge");
+    if (pledgeit.style.display === "block") {
+        pledgeit.style.display = "none";
+    } else {
+        pledgeit.style.display = "block"
+    }
+}
+function chosePledgeone() {
+    const pledgeitone = document.getElementById("selectPledgeone");
+    if (pledgeitone.style.display === "block") {
+        pledgeitone.style.display = "none";
+    } else {
+        pledgeitone.style.display = "block"
+    }
+}
+function chosePledgetwo() {
+    const pledgeittwo = document.getElementById("selectPledgetwo");
+    if (pledgeittwo.style.display === "block") {
+        pledgeitwo.style.display = "none";
+    } else {
+        pledgeittwo.style.display = "block"
+    }
+}
+function pledgedSupport() {
+    const pledge = document.getElementById("pledge-project");
+    if (pledge.style.display === "block") {
+        pledge.style.display = "none";
+    } else {
+        pledge.style.display = "block"
+    }
+}
